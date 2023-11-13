3d131c5f8bf39eafad7c531e23a4cb14c11401b5
torcuata22 <marilynjmarquez@gmail.com>
2023-11-13 15:50:03 -0500

    controller is working

---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,34 @@
+
+
+using BuberBreakfast.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+[ApiController]
+public class BreakfastsController : ControllerBase
+{
+    [HttpPost("/breakfasts")]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("/breakfasts/{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("/breakfasts/{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBrekfastRequest request)
+    {
+        return Ok(request);
+    }
+
+     [HttpDelete("/breakfasts/{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+}
